ee9fa9a230185f697f56ffd2c8fd5754bfa4c8ca
raj-bacancy <raj.patel@bacancy.com>
2021-03-31 10:22:14 +0530

    -

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Hdms
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.action_dispatch.default_headers.merge!({
